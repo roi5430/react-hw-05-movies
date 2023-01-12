a32bbcc195ce50822133d53e191822d9fd3b1b1c
kseniia <104863533+roi5430@users.noreply.github.com>
2023-01-12 22:58:32 +0200

    add files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,17 @@
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Routes>
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<Home />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="movies/:movieId" element={<Mission />}>
+            <Route path="cast" element={<Cast />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
     </div>
   );
 };
---
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from 'react-router-dom';
+
+export const SharedLayout = () => {
+  return (
+    <Container>
+      <Header>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/movies">Movies</Link>
+        </nav>
+      </Header>
+      <Outlet />
+    </Container>
+  );
+};
